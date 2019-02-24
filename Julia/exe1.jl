b9d16b8f5f82da1c494e0e62896d0e4f79824f39
Tatsunori Shiomoto <shiomoto-tatsunori-hz@ynu.jp>
2019-02-24 20:47:18 +0900

    Juliaの挙動確認

--- Julia/exe1.jl
+++ Julia/exe1.jl
@@ -1,5 +1,35 @@
-a = [1 2 3]
-b = [1: 2: 3]
-c = [1, 2, 3]
+#=
+This is a example code of Julia.
+exe1 program checks behavior of math calculation and String.
+=#
 
-print(a, b, c)
+a = 10 + 7
+println(a)
+
+b = 10 - 7
+println(b)
+
+c = 10 * 7
+println(c)
+
+d = 10 / 7
+println(d)
+
+#=
+str1 = 'string'
+println(str1, typeof(str1))
+=#
+
+str2 = "string"
+println(str2, typeof(str2))
+
+str3 = """string"""
+println(str3, typeof(str3))
+
+str4 = str2 * str3
+print(str4)
+
+while true
+    input = readline()
+    input == "0" && break
+end
