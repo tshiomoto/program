50631bc424bc49a88cd44a195c98409558a6d82d
Shiomoto-ynu <shiomoto-tatsunori-fr@ynu.jp>
2019-03-06 15:36:22 +0900

    FC1部分の作成，entropy関数の作成

--- Python/subic/encode.py
+++ Python/subic/encode.py
@@ -7,6 +7,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
+batch_num = 64
+
 '''
 サブベクトルに分割するメソッド
 input
@@ -14,44 +17,44 @@ input
     onehot: サブベクトルの長さ
     divide_num: サブベクトルの分割数
 output
-    output_cnnを[onehot, divide_num]に変形したデータ
+    output_cnnを[batch_num, onehot, divide_num]に変形したデータ
 '''
 def slice_vector(output_cnn, onehot, divide_num):
-    return output_cnn.reshape([onehot, divide_num])
+    return output_cnn.reshape([batch_num, onehot, divide_num])
 
 
 '''
 softmaxを計算するメソッド
 input
-    data: output_cnnを[onehot, divide_num]に変形したデータ
+    data: output_cnnを[batch_num, onehot, divide_num]に変形したデータ
 output
     dataをonehot方向でsoftmax関数かけたデータ
 '''
 def b_softmax(data):
-    softmax = F.softmax(dim=0)
+    softmax = F.softmax(dim=1)
     return softmax(data)
 
 
 '''
 log_softmaxを計算するメソッド
 input
-    data: output_cnnを[onehot, divide_num]に変形したデータ
+    data: output_cnnを[batch_num, onehot, divide_num]に変形したデータ
 output
     dataをonehot方向でlog_softmax関数かけたデータ
 '''
 def b_log_softmax(data):
-    log_softmax = F.log_softmax(dim=0)
+    log_softmax = F.log_softmax(dim=1)
     return log_softmax(data)
 
 '''
 onehotベクトルを生成するメソッド
 input
-    data: output_cnnを[onehot, divide_num]に変形したデータ
+    data: output_cnnを[batch_num, onehot, divide_num]に変形したデータ
 output
     dataをonehot方向でonehotベクトルにしたかけたデータ
 '''
 def make_onehot_vector(data):
-    predict_index_vector = torch.argmax(b_softmax(data), dim=0)
+    predict_index_vector = torch.argmax(b_softmax(data), dim=1)
     onehot_vector = torch.Tensor(np.zeros_like(data))
 
     for index in range(data.shape[0]):
@@ -62,9 +65,9 @@ def make_onehot_vector(data):
 '''
 blockwise_entropyを計算するメソッド
 input
-    data: output_cnnを[onehot, divide_num]に変形したデータ
+    data: output_cnnを[batch_num, onehot, divide_num]に変形したデータ
 output
-    entropy: [divide_num, ]のデータ，onehot方向にエントロピーを計算した値
+    entropy: [batch_num, divide_num]のデータ，onehot方向にエントロピーを計算した値
 '''
 def blockwise_entropy(data):
     entropy = b_softmax(data) * b_log_softmax(data)
@@ -72,12 +75,36 @@ def blockwise_entropy(data):
     return entropy
 
 '''
-全部のblockwise_entropyを計算し，その和を計算するメソッド
+1データごと全部のblockwise_entropyを計算し，その和を計算するメソッド
 input
-    data: output_cnnを[onehot, divide_num]に変形したデータ
+    data: output_cnnを[batch_num, onehot, divide_num]に変形したデータ
 output
-    blockwise_entropyの和を計算する
+    dataごとのblockwise_entropyの和を計算する
 '''
-def all_entropy(data):
+def tilde_entropy(data):
     entropy_vector = blockwise_entropy(data)
-    return entropy_vector.sum()
+    return entropy_vector.sum(axis=1)
+
+'''
+バッチデータ全部のentropyを計算するメソッド
+input
+    data: output_cnnを[batch_num, onehot, divide_num]に変形したデータ
+output
+    バッチデータ全部のblockwise_entropyの和を計算する
+'''
+def bar_entropy(data):
+    return F.mean(tilde_entropy(data))
+
+
+class LinearClassification(nn.Module):
+
+    def __init__(self):
+        super(LinearClassification, self).__init__()
+        self.fc = nn.Linear(1000, 100)
+
+
+    def __call__(self, x):
+        self.forward(x)
+
+    def forward(self, x):
+        return F.softmax(self.fc(x))
