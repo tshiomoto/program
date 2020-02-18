1f46001f2ae5460d9ed18a192c98b019febf876c
Shiomoto-ynu <ryuusyou3174@gmail.com>
2020-02-18 17:24:35 +0900

    作成中

--- Python/Django/src/test_moneys/models.py
+++ Python/Django/src/test_moneys/models.py
@@ -7,9 +7,30 @@ class Item(models.Model):
     # define name of the table
     class Meta:
         db_table = "Item"
+        verbose_name = "商品"
+        verbose_name_plural = "商品"
 
     # entities
-    item_name = models.CharField(max_length=100, unique=True)
+    item_name = models.CharField(verbose_name="品名", max_length=100, unique=True)
+    price = models.IntegerField(verbose_name="値段", help_text="単位は円")
+
+    def __str__(self):
+        return self.item_name
+
+
+class Person(models.Model):
+    
+    # define name of the table
+    class Meta:
+        db_table = "Person"
+        verbose_name = "購入者リスト"
+        verbose_name_plural = "購入者リスト"
+
+    # entities
+    person_name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.person_name
 
 
 class Buying(models.Model):
@@ -17,8 +38,14 @@ class Buying(models.Model):
     # define name of the table
     class Meta:
         db_table = "Buying"
+        verbose_name = "購入品リスト"
+        verbose_name_plural = "購入品リスト"
 
     # entities
     buy_date = models.DateField(verbose_name="日付", default=datetime.now)
-    item = models.ForeignKey(Item, on_delete = models.PROTECT, verbose_name="商品")
-    price = models.IntegerField(verbose_name="値段", help_text="単位は円")
+    buy_name = models.ForeignKey(Person, on_delete=models.PROTECT, verbose_name="購入者")
+    item_name = models.ForeignKey(Item, on_delete = models.PROTECT, verbose_name="商品")
+    number = models.IntegerField(verbose_name="個数", default=1)
+
+    def __str__(self):
+        return str(self.buy_name) + "_" + str(self.item_name)
--- Python/Django/src/test_moneys/views.py
+++ Python/Django/src/test_moneys/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Item, Person
 
 # Create your views here.
+def purchase(request):
+    item_list = Item.objects.order_by('-item_name')[:1]
+    person_list = Person.objects.order_by('-person_name')[:1]
+    context = {
+        "item_list": item_list,
+        "person_list": person_list,
+    }
+    return render(request, 'test_moneys/purchase.html', context)
