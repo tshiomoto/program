a6fd19431add9b3eee286dedb382099d96517512
Shiomoto-ynu <ryuusyou3174@gmail.com>
2020-02-18 18:27:23 +0900

    ビューの作成

--- Python/Django/src/test_moneys/views.py
+++ Python/Django/src/test_moneys/views.py
@@ -3,8 +3,8 @@ from .models import Item, Person
 
 # Create your views here.
 def purchase(request):
-    item_list = Item.objects.order_by('-item_name')[:1]
-    person_list = Person.objects.order_by('-person_name')[:1]
+    item_list = Item.objects.order_by('-item_name')
+    person_list = Person.objects.order_by('-person_name')
     context = {
         "item_list": item_list,
         "person_list": person_list,
